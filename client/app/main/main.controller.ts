6ca0a3357feea5b1dda5d4e21d6ef170e077dcdd
camendoz <cams.0093@gmail.com>
2016-03-14 20:54:50 -0600

    local.env.js

--- client/app/main/main.controller.ts
+++ client/app/main/main.controller.ts
@@ -9,9 +9,79 @@ class MainController {
     this.socket = socket;
     this.awesomeThings = [];
 
-    $scope.chartObject = {};
+    $scope.chartObject.data = {"cols": [
+      {
+        "name": "Medicina",
+        "timestamp": "",
+        "available": "100",
+        "total": 412,
+        "used": "200"
+      },
+        {
+          "name": "Visitantes",
+          "timestamp": "",
+          "available": "100",
+          "total": 237,
+          "used": "200",
+        },
+        {
+          "name": "Cafeteria/Centro de congresos",
+          "timestamp": "",
+          "available": "100",
+          "total": 288,
+          "used": "200"
+        },
+        {
+          "name": "Aulas VI/Residencias",
+          "timestamp": "",
+          "available": "100",
+          "total": 230,
+          "used": "200"
+        },
+        {
+          "name": "Aulas V/Ingenieria Civil",
+          "timestamp": "",
+          "available": "100",
+          "total": 291,
+          "used": "200"
+        },
+        {
+          "name": "Zona Azul",
+          "timestamp": "",
+          "available": "100",
+          "total": 229,
+          "used": "200"
+        },
+        {
+          "name": "Zona Verde",
+          "timestamp": "",
+          "available": "100",
+          "total": 270,
+          "used": "200"
+        },
+        {
+          "name": "Zona Naranja",
+          "timestamp": "",
+          "available": "100",
+          "total": 269,
+          "used": "200"
+        },
+        {
+          "name": "Zona Amarilla",
+          "timestamp": "",
+          "available": "100",
+          "total": 341,
+          "used": "200"
+        },
+        {
+          "name": "Zona Morada",
+          "timestamp": "",
+          "available": "100",
+          "total": 446,
+          "used": "200"
+        }]};
 
-    $scope.chartObject.type = "ColumnChart";
+
 
     $scope.onions = [
       {v: "Onions"},
@@ -40,6 +110,8 @@ class MainController {
         {v: 2},
       ]}
     ]};
+
+
 
     $scope.chartObject.options = {
       'title': 'How Much Pizza I Ate Last Night'
