a4e5d67c4cf347b11e82dd3388c0027868de445a
camendoz <cams.0093@gmail.com>
2016-03-17 21:26:51 -0600

    comment login text

--- server/config/local.env.js
+++ server/config/local.env.js
@@ -6,7 +6,7 @@
 // You will need to set these on the server you deploy to.
 
 module.exports = {
-  DOMAIN: 'http://104.236.115.243:9000',
+  DOMAIN: 'http://localhost:9000',
   SESSION_SECRET: 'gubiparking-secret',
 
   FACEBOOK_ID: '204789119884848',
